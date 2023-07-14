979acd862465e7271c34fdb278b8d020e0516a87
Matina Shrestha <shresthamatina21@gmail.com>
2023-07-14 15:33:20 -0500

    React Native Components Part 2

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -3,7 +3,7 @@ import { Avatar, ListItem } from "react-native-elements";
 const DirectoryScreen = (props) => {
   const renderDirectoryItem = ({ item: campsite }) => {
     return (
-      <ListItem>
+      <ListItem onPress={() => props.onPress(campsite.id)}>
         <Avatar source={campsite.image} rounded />
         <ListItem.Content>
           <ListItem.title>{campsite.name}</ListItem.title>
